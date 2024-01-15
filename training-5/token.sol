4a404722a7457d4faf4d839f30ce8dd9c95549ee
Oyeniyi Abiola Peace <38940247+iamoracle@users.noreply.github.com>
2024-01-15 06:56:43 +0100

    text formatted

--- training-5/token.sol
+++ training-5/token.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract BIHToken is ERC20 {
     constructor()
-        ERC20("BIHToken", "BIH")
+        ERC20("BIH Token", "BIH")
     {
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
