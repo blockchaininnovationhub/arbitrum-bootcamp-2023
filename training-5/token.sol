9bfdbb32d41bcab766db43b73dc5351f7a40129e
Ray King <rayodo667@gmail.com>
2024-01-15 06:32:12 +0100

    chore: add token source code

--- training-5/token.sol
+++ training-5/token.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BIHToken is ERC20, Ownable {
+    constructor(address initialOwner)
+        ERC20("BIHToken", "BIH")
+        Ownable(initialOwner)
+    {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
