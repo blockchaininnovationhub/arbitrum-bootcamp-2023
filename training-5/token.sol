298c27aff7eada9bbd817c51fb713b96afa7bfff
Oyeniyi Abiola Peace <38940247+iamoracle@users.noreply.github.com>
2024-01-15 06:56:15 +0100

    removed ownable

--- training-5/token.sol
+++ training-5/token.sol
@@ -2,17 +2,11 @@
 pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BIHToken is ERC20, Ownable {
-    constructor(address initialOwner)
+contract BIHToken is ERC20 {
+    constructor()
         ERC20("BIHToken", "BIH")
-        Ownable(initialOwner)
     {
         _mint(msg.sender, 1000000 * 10 ** decimals());
-    }
-
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
     }
 }
