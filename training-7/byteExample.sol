316ce76096414e856c10ce7f2ad9820919527245
Oyeniyi Abiola Peace <38940247+iamoracle@users.noreply.github.com>
2024-01-22 11:22:37 +0100

    Create byteExample.sol

---
+++ training-7/byteExample.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+
+contract BytesExample {
+
+    bytes2 a = 0xafff; 
+
+    string b = "ajahjahhshjshjsghsghsghsghsghsghsghsghghghj";
+
+    bytes c = "axzzzzzzzzz";
+
+    bytes32 d = "abcdefghijabcdefghijabcdefghijaa";
+
+}
