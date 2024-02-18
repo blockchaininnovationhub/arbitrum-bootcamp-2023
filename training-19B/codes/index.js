1087ec29f335ffca0e7b0e9b9bccc639f1c5cdf4
Oyeniyi Abiola Peace <38940247+iamoracle@users.noreply.github.com>
2024-02-18 22:38:50 +0100

    Update index.js

---
+++ training-19B/codes/index.js
@@ -0,0 +1,8 @@
+let http = require('http');
+
+http.createServer(function (req, res) {
+  res.write('Hello World!');
+  res.end();
+}).listen(3000, function(){
+ console.log("server start at port 3000");
+});
